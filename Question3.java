a591998979aab30f28f42dc3f4e5572fd0a4eaa1
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-19 15:27:13 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -12,5 +12,10 @@ public class Question3
      
     Scanner in = new Scanner(System.in);
     
+    int num = in.nextInt();
+    int result = num * num;
+    
+    System.out.println(result);
+    
   }
 }
